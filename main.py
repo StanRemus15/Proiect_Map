35b7edeec82a2da44dbf699de73760194a540986
Stan Remus <stanremus2004@yahoo.com>
2024-11-24 15:23:59 +0200

    Implementare functie pentru generare de turnuri in mod aleatoriu

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import matplotlib.pyplot as pl
 import numpy as npy
 import random
+
+def generare_turnuri(n):
+    tabla_sah = []
+    for i in range(n):
+        linie = [0]*n
+        tabla_sah.append(linie)
+    coloane_ocupate = set()
+    for linie in range(n):
+        coloane_libere=[]
+        for coloane in range(n):
+            if coloane not in coloane_ocupate:
+                coloane_libere.append(coloane)
+        coloane=random.choice(coloane_libere)
+        tabla_sah[linie][coloane]=1
+        coloane_ocupate.add(coloane)
+    return tabla_sah
